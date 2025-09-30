b561e4f64d188dc77fd0e8a09090e5ae97ff0554
kalzcool <nani.carrillo@ucas-edu.net>
2025-09-30 14:11:36 -0600

    finished hangman final
    Co-authored-by: programming <adalynnblackburn@users.noreply.github.com>
    Co-authored-by: ryanaguilera-commits <ryanaguilera-commits@users.noreply.github.com>
    Co-authored-by: KatKat0729 <KatKat0729@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,69 +1,59 @@
 # CC RA KW AB 7th
-import sys
-# Hangman (python final) 
+
+#Hangman Python Final
 
 #CC inputs letters and words
 
 #RA word order
 
-#KW looping the game and lists
+#KW looping the game and lists, making the code look nice
 
 #AB conditionals
 
 #CC
-game_start = input("would you like to play hangman?\n").strip().title()
-#AB
-if game_start == "Yes":
-    print("Welcome to the game!")
-else:
-    print("Goodbye!") 
-    sys.exit()
-    
+import sys
 import random
 
-#RA KW AB CC
-words = ["cactus", "cactus"]
-
-#CC 
-
-chosen_word = random.choice(words)
-#RA
-def hangman():
-    chosen_word = random.choice(words)
-    random_item_list = list(chosen_word)
-    word_length = len(chosen_word)
-    display = ["_" ]
-    for _ in range(word_length):
-        display += "_"
-    print(display)
-end_of_game = False
-lives = 6
-#CC
+word_list = ["milky", "zigzag", "foggy", "mummy", "jolly", "poppy", "giggle", "happily", "fluffy", "halfway", "hedgehog", "kick", "chipmunk", "kabaddi", "clockwork", "lazy", "juicy", "chuckled", "foxglove", "vuvuzela", "plump", "chuck", "fully", "puff", "jacuzzi", "middle", "magnify", "black", "pack", "punch", "publicly", "dumbbell", "hobby", "apply", "quack", "bluebell", "crazy", "block", "puppy", "quiz", "pick", "gulf", "lucky", "clock", "cyborg", "prickly", "clump", "skull", "buzzer", "jugs", "skillful", "cyberpunk", "puzzle", "symbol", "chef", "windmill", "lollipop", "pumpkin", "building", "yolk", "cloud", "wrongly", "blazing", "fury", "clumsy", "pluck", "awful", "church", "yacht", "furlough", "duck", "muddy", "padlock", "flag", "headache", "flapjack", "unhappy", "lifestyle", "globe",  "decided", "cycling", "awkward", "gypsy", "symbols", "hyperlink", "pebble", "club", "baby", "volume", "review", "puzzling", "quickly", "pudding", "daffodil", "maximum", "suck"]
+word = random.choice(word_list)
+lives = 7
 guessed_letters = []
-hangman()
-
-def show_words(guessed_letters, lives = 6): 
-    guess = input("give me a letter fine sir:\n").lower()
-    guessed_letters.append(guess)
+#RA
+def display_word():
+    display = " _ "
 #KW
-    chosen_word = random.choice(words)
-    if guess in chosen_word:
-         print(f"You guessed correctly, you have {lives} lives left.")
-         
+    for letter in word:
+        if letter in guessed_letters: # If a letter is guessed it will display it, but if its not, it will display _. It does this for every letter in the word.
+            print(letter, end="") # Printing correctly guessed letter, replacing the _.
+        else:            
+            print(display, end="") # Printing "_" because the letter hasn't been guessed.
+    print(" ")
+#RA
+def did_guess_all_letters():
+#KW
+    for letter in word:
+        if letter not in guessed_letters:
+            return False
+    return True
+# Start of game loop
+#KW
+while lives >= 1:
+    print(f"You have {lives} lives left.")
+    display_word()
+    guess = input("Guess a letter - \n").strip().lower()
+#AB
+    if len(guess) > 1:
+        print("Stop wasting my time.")
+        sys.exit() 
+    guessed_letters.append(guess) #append adds something to a list (We are adding the guess to the list of guessed letters)
+    if guess in word:
+        print(f"Correct.")
     else:
         lives -= 1
-        print(f"You guessed incorrectly, you have {lives} lives left.")
-        if lives == 0 :
-                print("You loose")
-                sys.exit()
-
-   #CC
-
-show_words(guessed_letters)
-show_words(guessed_letters, lives = 5)
-show_words(guessed_letters, lives = 5)
-show_words(guessed_letters, lives = 5)
-show_words(guessed_letters, lives = 5)
-show_words(guessed_letters, lives = 5)
-
-    
+        print(f"Incorrect.")
+    if did_guess_all_letters():
+        print(f"Congratulations, you won with {lives} lives left!")
+        sys.exit()
+#CC
+print(f"You don't have any more guesses, you lose. The word was {word}")
+sys.exit()
